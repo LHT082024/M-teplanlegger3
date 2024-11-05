573eacdc23411323b96673c648bf8f9b2f05cdb5
LHT082024 <LHT082024@gmail.com>
2024-11-05 12:36:18 +0100

    starting bascs

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Møteplanlegger3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ classes/MeetingClass.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Møteplanlegger3.classes
+{
+    public class MeetingClass
+    {
+        
+    }
+}
